d0759a4ae86453b9b856da74c7039c9fc6adc0da
ALESSANDRO EMANUELLE GRACIA ALBAN <alessandro.gracia@pucese.edu.ec>
2020-11-15 23:13:02 -0500

    Agregar archivos de proyecto.

---
+++ Tarea de Funciones2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EstructuraCondicionalSimple1
+{
+    class Program
+    {
+        //Ingresar el sueldo de una persona, si supera los 3000 pesos mostrar un mensaje en pantalla indicando que debe abonar impuestos.
+        // Tarea: Preparar un ejercicio que dentro de una funcion use  un condicional
+        static void Main(string[] args)
+        {
+            float sueldo;
+            string linea;
+            Console.Write("Ingrese el sueldo:");
+            linea = Console.ReadLine();
+            sueldo = float.Parse(linea);
+            if (sueldo > 3000)
+            {
+                Console.Write("Esta persona debe abonar impuestos");
+            }
+            Console.ReadKey();
+        }
+    }
+}
